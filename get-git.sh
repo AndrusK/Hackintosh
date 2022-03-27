af65907e7efb435a372db03abab4d8250fa2ad79
AndrusK <bhayes.dev@hotmail.com>
2022-03-27 02:42:53 -0400

    Create get-git.sh

---
+++ get-git.sh
@@ -0,0 +1,6 @@
+cd ~/Documents
+git clone https://github.com/racka98/Lenovo-Thinkpad-T450-T450s-Hackintosh-Guide-Opencore
+git clone https://github.com/corpnewt/MountEFI
+git clone https://github.com/corpnewt/ProperTree
+git clone https://github.com/CLAY-BIOS/Lenovo-ThinkPad-T450s-Hackintosh-OpenCore
+git clone https://github.com/corpnewt/GenSMBIOS
